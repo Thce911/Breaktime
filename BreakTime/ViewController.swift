a555da0d129fe5d68791d002d936e62320c39445
Heriberto <11hce@itesm.mx>
2019-04-08 14:52:00 -0500

    upgrade score engine

--- BreakTime/ViewController.swift
+++ BreakTime/ViewController.swift
@@ -9,34 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+   
     var score1 = 0
     var score2 = 0
-    
+   
     @IBOutlet weak var substracttwobtn: UIButton!
     @IBOutlet weak var addtwobtn: UIButton!
     @IBOutlet weak var twoscrlbl: UILabel!
     @IBOutlet weak var substractonebtn: UIButton!
     @IBOutlet weak var onescrlbl: UILabel!
     @IBOutlet weak var addonebtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+       
     }
 
     @IBAction func addonebtn(_ sender: Any) {
         score1 += 1
         onescrlbl.text = String(score1)
-        if score1 == 10 {
-            // create the alert
-            let alert = UIAlertController(title: "Matchpoint!", message: "Tienes 4 saques", preferredStyle: UIAlertController.Style.alert)
-            
-            // add an action (button)
-            alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
-            
-            // show the alert
-            self.present(alert, animated: true, completion: nil)
-        }
+        isMatchpoint(scoreA: score1, scoreB: score2)
     }
     
     @IBAction func substractonebtn(_ sender: Any) {
@@ -46,11 +38,27 @@ class ViewController: UIViewController {
     @IBAction func addtwobtn(_ sender: Any) {
         score2 += 1
         twoscrlbl.text = String(score2)
+        isMatchpoint(scoreA: score1, scoreB: score2)
+        
         
     }
     @IBAction func substracttwobtn(_ sender: Any) {
         score2 -= 1
         twoscrlbl.text = String(score2)
+    }
+    
+    func isMatchpoint(scoreA: Int, scoreB: Int){
+        if scoreA >= 9 && scoreB >= 9{
+            if scoreA - scoreB != 0 {
+                let alert = UIAlertController(title: "Matchpoint!", message: "Tienes 4 saques", preferredStyle: UIAlertController.Style.alert)
+                
+                // add an action (button)
+                alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+                
+                // show the alert
+                self.present(alert, animated: true, completion: nil)
+            }
+        }
     }
 }
 
